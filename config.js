431dab3f4bc1483caa7aceaa3a824fdbcd915972
Michael Cloutier <mcloutier@clarityservices.com>
2015-01-28 08:45:12 -0500

    Update settings

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://mjcloutier.github.io',
         mail: {},
         database: {
             client: 'sqlite3',
